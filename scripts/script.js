262d89d65580401b2d30aeb13cf8bde2abfa1d66
Margarita Kolyasnikova <margarita.kolyasnikova@gmail.com>
2021-12-22 15:41:43 +0500

    fix: fix modif & line

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@ const popupOpenButton = document.querySelector('.profile__edit-button');
 const popupForm = document.querySelector('.popup');
 const popupCloseButton = popupForm.querySelector('.popup__close-button');
 const formElement = popupForm.querySelector('.popup__form'); 
-const nameInput = popupForm.querySelector('.popup__input_name'); 
-const jobInput = popupForm.querySelector('.popup__input_job');
+const nameInput = popupForm.querySelector('.popup__input_type_name'); 
+const jobInput = popupForm.querySelector('.popup__input_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
