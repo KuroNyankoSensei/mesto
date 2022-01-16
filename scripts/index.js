60953b35881e99cf8d3607006f6d706fa28d95b6
Margarita Kolyasnikova <madnightmoon@gmail.com>
2022-01-16 17:17:51 +0500

    fix/ review comments

--- scripts/index.js
+++ scripts/index.js
@@ -37,10 +37,6 @@ const addCardButton = document.querySelector('.profile__add-button');
 const addCardCloseButton = popupAddCard.querySelector('.popup__close-button');
 const closeImageButton = popupImage.querySelector('.popup__close-button');
 
-function togglePopup(popup) {
-  popup.classList.toggle('popup_opened');
-};
-
 //Forms
 const editFormElement = popupProfileEdit.querySelector('.popup__form');
 const addFormElement = popupAddCard.querySelector('.popup__form');
@@ -51,25 +47,57 @@ const jobInput = popupProfileEdit.querySelector('.popup__input_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
 
-profileEditButton.addEventListener('click', () => {
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-  togglePopup(popupProfileEdit)
-});
-profileCloseButton.addEventListener('click', () => togglePopup(popupProfileEdit));
-editFormElement.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  togglePopup(popupProfileEdit);
-});
-
 //List template
 const list = document.querySelector('.photos__list');
 const cardTemplate = document.querySelector('.card-template').content;
 const popupCardImage = document.querySelector('.popup__image');
 const popupCardTitle = document.querySelector('.popup__image-description');
 
+//Add card
+const placeNameInput = popupAddCard.querySelector('.popup__input_type_place-name');
+const linkInput = popupAddCard.querySelector('.popup__input_type_link');
+
+//open close popup
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+};
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+};
+
+//edit profile
+function inputInfo() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+};
+profileEditButton.addEventListener('click', () => inputInfo());
+profileEditButton.addEventListener('click', () => openPopup(popupProfileEdit));
+profileCloseButton.addEventListener('click', () => closePopup(popupProfileEdit));
+editFormElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup(popupProfileEdit);
+});
+
+//add card
+addCardButton.addEventListener('click', () => openPopup(popupAddCard));
+addCardCloseButton.addEventListener('click', () => closePopup(popupAddCard));
+
+addFormElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  renderPlaceCard({
+    name: placeNameInput.value,
+    link: linkInput.value
+  });
+  addFormElement.reset();
+  closePopup(popupAddCard);
+});
+
+//Close modal image
+closeImageButton.addEventListener('click', () => closePopup(popupImage));
+
+//create card
 function createCard(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImage = cardElement.querySelector('.photos__image');
@@ -90,7 +118,7 @@ function createCard(cardData) {
   });
 
   cardImage.addEventListener('click', () => {
-    togglePopup(popupImage);
+    openPopup(popupImage);
     popupCardImage.src = cardData.link;
     popupCardImage.alt = cardData.name;
     popupCardTitle.textContent = cardData.name;
@@ -105,22 +133,3 @@ const renderPlaceCard = (data) => {
 };
 
 initialCards.forEach(renderPlaceCard);
-
-//Add card
-const placeNameInput = popupAddCard.querySelector('.popup__input_type_place-name');
-const linkInput = popupAddCard.querySelector('.popup__input_type_link');
-
-addCardButton.addEventListener('click', () => togglePopup(popupAddCard));
-addCardCloseButton.addEventListener('click', () => togglePopup(popupAddCard));
-
-addFormElement.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  renderPlaceCard({
-    name: placeNameInput.value,
-    link: linkInput.value
-  });
-  togglePopup(popupAddCard);
-});
-
-//Close modal image
-closeImageButton.addEventListener('click', () => togglePopup(popupImage));
